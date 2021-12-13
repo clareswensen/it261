b1dd717496f754b028ca2188f22fb8a69a3e8f7e
Clare <81480773+clareswensen@users.noreply.github.com>
2021-12-13 14:53:16 -0700

    Delete credentials.php

--- weeks/week9/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_NAME', 'clareswensendesigns_fall2021');
-define('DB_USER', 'myfall2021_user');
-define('DB_PASSWORD', 'mysqluser2021');
-define('DB_HOST', 'mysql.clareswensendesigns.com');
-
-?>
