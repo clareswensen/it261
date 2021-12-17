0d7756d45811d753563d8982f5458b00dacb2cf0
Clare <81480773+clareswensen@users.noreply.github.com>
2021-12-16 23:04:15 -0700

    Add files via upload

--- final/includes/form.php
+++ final/includes/form.php
@@ -1,20 +1,20 @@
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
         <fieldset>
         <label for="first_name">First Name</label>
-        <input type="text" name="first_name" value="<?php if(isset($_POST['first_name'])) echo htmlspecialchars($_POST['first_name']);?>">
+        <input type="text" id="first_name" name="first_name" value="<?php if(isset($_POST['first_name'])) echo htmlspecialchars($_POST['first_name']);?>">
         <span class="error"><?php echo $first_name_Err;?></span>
 
         <label for="last_name">Last Name</label>
-        <input type="text" name="last_name" value="<?php if(isset($_POST['last_name'])) echo htmlspecialchars($_POST['last_name']);?>">
+        <input type="text" id="last_name" name="last_name" value="<?php if(isset($_POST['last_name'])) echo htmlspecialchars($_POST['last_name']);?>">
         <span class="error"><?php echo $last_name_Err;?></span>
 
         <label for="email">Email</label>
-        <input type="email" name="email" value="<?php if(isset($_POST['email'])) echo htmlspecialchars($_POST['email']);?>">
+        <input type="email" id="email" name="email" value="<?php if(isset($_POST['email'])) echo htmlspecialchars($_POST['email']);?>">
         <span class="error"><?php echo $email_Err;?></span>
 
         <label for="reason">What email updates are you interested in?</label>
         <ul>
-        <li><input type="radio" name="reason" value="recipes" <?php if(isset($_POST['reason']) && $_POST['reason'] == 'recipes') echo 'checked="checked"';?>> recipes</li>
+        <li><input type="radio" id="reason" name="reason" value="recipes" <?php if(isset($_POST['reason']) && $_POST['reason'] == 'recipes') echo 'checked="checked"';?>> recipes</li>
         <li><input type="radio" name="reason" value="identification tips" <?php if(isset($_POST['reason']) && $_POST['reason'] == 'identification tips') echo 'checked="checked"';?>> identification tips</li>
         <li><input type="radio" name="reason" value="new_products" <?php if(isset($_POST['reason']) && $_POST['reason'] == 'new_products') echo 'checked="checked"';?>> new products</li>
         </ul>
@@ -23,7 +23,7 @@
 
         <label for="mushrooms">Favorite wild mushrooms:</label>
         <ul>
-        <li><input type="checkbox" name="mushrooms[]" value="morel" <?php if(isset($_POST['mushrooms']) && in_array('morel',$mushrooms)) echo 'checked="checked"';?>> Morel</li>
+        <li><input type="checkbox" id="mushrooms" name="mushrooms[]" value="morel" <?php if(isset($_POST['mushrooms']) && in_array('morel',$mushrooms)) echo 'checked="checked"';?>> Morel</li>
         <li><input type="checkbox" name="mushrooms[]" value="chanterelle" <?php if(isset($_POST['mushrooms']) && in_array('chanterelle',$mushrooms)) echo 'checked="checked"';?>> Chanterelle</li>
         <li><input type="checkbox" name="mushrooms[]" value="porcini" <?php if(isset($_POST['mushrooms']) && in_array('porcini',$mushrooms)) echo 'checked="checked"';?>> Porcini</li>
         <li><input type="checkbox" name="mushrooms[]" value="oyster" <?php if(isset($_POST['mushrooms']) && in_array('oyster',$mushrooms)) echo 'checked="checked"';?>> Oyster</li>
@@ -32,7 +32,7 @@
         <span class="error"><?php echo $mushrooms_Err;?></span>
 
         <label for="locale">Where do you live?</label>
-        <select name="locale">
+        <select name="locale" id="locale">
         <option value="" <?php if(isset($_POST['locale']) && $_POST['locale'] == NULL) echo 'selected="unselected"';?>>Select One!</option>
         <option value="pnw" <?php if(isset($_POST['locale']) && $_POST['locale'] == 'pnw') echo 'selected="unselected"';?>> PNW</option>
         <option value="europe" <?php if(isset($_POST['locale']) && $_POST['locale'] == 'europe') echo 'selected="unselected"';?>> Europe</option>
@@ -42,7 +42,7 @@
         <span class="error"><?php echo $locale_Err;?></span>
 
         <label for="comments">Comments:</label>
-        <textarea name="comments"><?php if(isset($_POST['comments'])) echo htmlspecialchars($_POST['comments']);?></textarea>
+        <textarea name="comments" id="comments"><?php if(isset($_POST['comments'])) echo htmlspecialchars($_POST['comments']);?></textarea>
         <span class="error"><?php echo $comments_Err;?></span>
 
 
